8f079551174c9add3bf79f13c3941ba64d2b2573
tkdals5879 <tkdals58799@gmail.com>
2025-04-18 11:34:31 +0900

    모달창 구현

---
+++ src/component/Modal.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+
+import '../css/modal/modal.css'
+
+function Modal({clickedTeam,handleModalClose}) {
+
+    const teams = useSelector((state) => state.teams.teams)
+    const teamName = clickedTeam.name
+    console.log(teamName)
+
+    const handleBgClick = (e) => {
+        if(e.target.className === 'modalBg'){
+            handleModalClose()
+        }
+    }
+
+    return (
+        <div className='modalBg' onClick={handleBgClick}>
+            <div className='modalWrap'>
+                <h3>{teamName}</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Modal;
--- src/pages/StadiumDetail.js
+++ src/pages/StadiumDetail.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import Modal from '../component/Modal';
 
 import '../css/stadiumDetail/stadiumDetail.css'
-
 
 function StadiumDetail() {
 
@@ -11,8 +11,20 @@ function StadiumDetail() {
     const teams = useSelector((state) => state.teams.teams)
     const clickedTeam = teams.find(team => team.id === teamId)
 
-    if (!clickedTeam) return <p>팀을 찾을 수 없습니다!</p>
+    
+    const [modalOpen,setModalOpen] = useState(false)
+    
+    const handleModalOpen = () => {
+        setModalOpen(true)
+    }
 
+    const handleModalClose = () => {
+        setModalOpen(false)
+    }
+    
+    useEffect(() => {console.log("모달 상태 > ", modalOpen)},[modalOpen])
+    
+    if (!clickedTeam) return <p>팀을 찾을 수 없습니다!</p>
 
     return (
         <div className='stadiumDetailWrap'>
@@ -22,21 +34,34 @@ function StadiumDetail() {
                     <h1>{clickedTeam.stadiumName}</h1>
                     <p>{clickedTeam.stadiumOpen}</p>
                 </div>
-                <div className='stadiumPicture'></div>
+                <div className='stadiumPicture'>
+                    <img src={clickedTeam.stadiumImg} alt={clickedTeam.stadiumName} />
+                </div>
                 <div className='homeTeamInfo'>
-                    <div className='homeTeamInfoLeft'>
-                        <img src={clickedTeam.logo} alt={clickedTeam.homeTeam} />
-                    </div>
-                    <div className='homeTeamInfoRight'>
-                        <h3>{clickedTeam.homeTeam}</h3>
-                        {/* 경기실적 넣기 */}
+                    <h2>홈 구단 정보</h2>
+                    <div>
+                        <div className='homeTeamInfoLeft'>
+                            <img src={clickedTeam.logo} alt={clickedTeam.homeTeam} />
+                        </div>
+                        <div className='homeTeamInfoRight'>
+                            <h3>{clickedTeam.homeTeam}</h3>
+                            <p>전적</p>
+                            <p>승률</p>
+                            <span onClick={handleModalOpen}>더보기</span>
+                            {/* 경기실적 넣기 */}
+                        </div>
                     </div>
                 </div>
             </div>
 
             <div className='location'>
-                
+                <h2>위치</h2>
+                <iframe src={clickedTeam.iframeUrl}></iframe>
             </div>
+
+            {modalOpen && (
+                <Modal clickedTeam={clickedTeam} handleModalClose={handleModalClose}/>
+            )}
 
         </div>
     )
